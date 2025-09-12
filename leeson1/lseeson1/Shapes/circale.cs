ec6a656ddbb754504bcffacf94c004e42452d1b7
Esti1644 <Hand1e19@mbjcomp.org.il>
2025-09-12 09:47:29 +0300

    Add files via upload

---
+++ leeson1/lseeson1/Shapes/circale.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class circale
+    {
+        public double scoup{ get; set; }
+        public string color { get; set; }
+
+        public circale(double scoup, string color)
+        {
+            this.scoup = scoup;
+            this.color = color;
+        }
+    }
+}
---
+++ leeson1/lseeson1/lesson1/main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shapes;
+
+namespace lesson1
+{
+    public class Class1
+    {
+        static void Main(string[] args) {
+            circale[] circales = new circale[4];
+            Rectangle[] rectangles = new Rectangle[8];
+            Rectangle[] rectangles1 = new Rectangle[8];
+            foreach (circale item in circales) {
+                if (item.scoup>7)
+                    Console.WriteLine(item.scoup+" "+item.color);
+
+    }
+            var rectangle2=from item in rectangles
+                           where item.length<5
+                           select item;
+
+        }
+    }
+}
